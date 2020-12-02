70e6b224ad071dd32a00800f4131aeaf1889058e
Vitor Guizilini <vitor.guizilini@tri.global>
2020-12-02 15:41:48 -0800

    release

--- camviz/utils/geometry.py
+++
@@ -1,9 +0,0 @@
-
-import numpy as np
-
-def unitX(m=1.0): return np.array((m, 0, 0))
-def unitY(m=1.0): return np.array((0, m, 0))
-def unitZ(m=1.0): return np.array((0, 0, m))
-
-def transpose(data): return data.T
-def invert(data): return np.linalg.inv(data)
